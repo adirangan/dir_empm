c3c024faca503b2a0c750aaf771d371e61863004
Aaditya Rangan <rangan@math.nyu.edu>
2026-03-19 15:24:46 -0400

    test_principled_marching_cost_matrix_static_0

--- test/dir_interaction_required/test_principled_marching_cost_matrix_static_0.py
+++ test/dir_interaction_required/test_principled_marching_cost_matrix_static_0.py
@@ -32,7 +32,7 @@ M_k_p_wkM__ = torch.reshape(torch.tensor(np.mod(np.arange(n_w_sum*n_M),13)-6).to
 #%%%%%%%%;
 
 #%%%%%%%%;
-#% disp(num2str(X0_kk__(:),';%+0.16f ...\n'))
+#% disp(num2str(X0_kk__(:),';%+0.16f ...\n'));
 #%%%%%%%%;
 static_X0_kk__ = torch.tensor([
     +4.0089928109038837,
@@ -92,7 +92,7 @@ pm_delta_integral_tolerance = 1e-9;
 #%%%%%%%%;
 
 #%%%%%%%%;
-#% disp(num2str(X_kk__(:),';%+0.16f ...\n'))
+#% disp(num2str(X_kk__(:),';%+0.16f ...\n'));
 #%%%%%%%%;
 static_X_kk__ = torch.tensor([
     +0.8779589809787692,
@@ -115,7 +115,7 @@ static_X_weight_r_ = torch.tensor([
 ]).to(dtype=torch.float32);
 fnorm_disp(flag_verbose,'static_X_weight_r_',static_X_weight_r_,'X_weight_r_',X_weight_r_,'%<-- should be zero');
 #%%%%%%%%;
-#% disp(num2str(X_ori_kk__(:),';%+0.16f ...\n'))
+#% disp(num2str(X_ori_kk__(:),';%+0.16f ...\n'));
 #%%%%%%%%;
 static_X_ori_kk__ = torch.tensor([
     +0.0055598288484369 + +0.0000000000000000*i,
@@ -131,7 +131,7 @@ static_X_ori_kk__ = torch.tensor([
 static_X_ori_kk__ = torch.reshape(static_X_ori_kk__,mtr((3,3)));
 fnorm_disp(flag_verbose,'static_X_ori_kk__',static_X_ori_kk__,'X_ori_kk__',X_ori_kk__,'%<-- should be zero');
 #%%%%%%%%;
-#% disp(num2str(X_tau_kk__(:),';%+0.16f ...\n'))
+#% disp(num2str(X_tau_kk__(:),';%+0.16f ...\n'));
 #%%%%%%%%;
 static_X_tau_kk__ = torch.tensor([
     +0.0414976441478332 + +0.0000000000000000*i,
@@ -187,7 +187,7 @@ pm_delta_integral_tolerance = 1e-9;
 #%%%%%%%%;
 
 #%%%%%%%%;
-#% disp(num2str(X_kk__(:),';%+0.16f ...\n'))
+#% disp(num2str(X_kk__(:),';%+0.16f ...\n'));
 #%%%%%%%%;
 static_X_kk__ = torch.tensor([
     +0.8779589809787692,
@@ -210,7 +210,7 @@ static_X_weight_r_ = torch.tensor([
 ]).to(dtype=torch.float32);
 fnorm_disp(flag_verbose,'static_X_weight_r_',static_X_weight_r_,'X_weight_r_',X_weight_r_,'%<-- should be zero');
 #%%%%%%%%;
-#% disp(num2str(X_ori_kk__(:),';%+0.16f ...\n'))
+#% disp(num2str(X_ori_kk__(:),';%+0.16f ...\n'));
 #%%%%%%%%;
 static_X_ori_kk__ = torch.tensor([
     +0.0055598288484369 + 0.0000000000000000*i,
@@ -226,7 +226,7 @@ static_X_ori_kk__ = torch.tensor([
 static_X_ori_kk__ = torch.reshape(static_X_ori_kk__,mtr((3,3)));
 fnorm_disp(flag_verbose,'static_X_ori_kk__',static_X_ori_kk__,'X_ori_kk__',X_ori_kk__,'%<-- should be zero');
 #%%%%%%%%;
-#% disp(num2str(X_tau_kk__(:),';%+0.16f ...\n'))
+#% disp(num2str(X_tau_kk__(:),';%+0.16f ...\n'));
 #%%%%%%%%;
 static_X_tau_kk__ = torch.tensor([
 +0.0414976441478332 + 0.0000000000000000*i,
@@ -238,6 +238,102 @@ static_X_tau_kk__ = torch.tensor([
 +0.4169780925489920 + 0.0276813696844958*i,
 +0.9160336431466161 - 0.6763041939424745*i,
 +4.2083590883200568 + 0.0000000000000000*i,
+]).to(dtype=torch.complex64);
+static_X_tau_kk__ = torch.reshape(static_X_tau_kk__,mtr((3,3)));
+fnorm_disp(flag_verbose,'static_X_tau_kk__',static_X_tau_kk__,'X_tau_kk__',X_tau_kk__,'%<-- should be zero');
+static_weight_so3 = 157.9136704174297279 ; #%<-- should be (2*pi)*(2*pi)*4 ;
+fnorm_disp(flag_verbose,'static_weight_so3',static_weight_so3,'weight_so3',weight_so3,'%<-- should be zero');
+
+
+disp(sprintf(' %% %% %% %% %% %% %% %% %% %% %% %% %% %% %% %% '));
+disp(sprintf(' %% testing principled_marching_empirical_cost_matrix_2 again, this time with CTF_k_p_r_xcor_kk__ as rank-1 and only a single reference molecule'));
+disp(sprintf(' %% %% %% %% %% %% %% %% %% %% %% %% %% %% %% %% '));
+n_k_p_r = int(3);
+k_p_r_ = torch.tensor([3,5,7]).to(dtype=torch.float32);
+weight_k_p_r_ = torch.tensor([0.25,0.50,0.75]).to(dtype=torch.float32)**2;
+l_max_ = torch.tensor([2,4,6]).to(dtype=torch.int32);
+n_y_ = (1+l_max_)**2;
+n_y_sum = int(torch.sum(n_y_).item());
+n_molecule = int(1);
+molecule_density_ = torch.tensor([1.0]).to(dtype=torch.float32);
+a_k_Y_ykv__ = torch.reshape(torch.tensor(np.mod(np.arange(n_y_sum*n_molecule),13)-6).to(dtype=torch.complex64) + i*torch.tensor(np.mod(np.arange(n_y_sum*n_molecule),17)-8).to(dtype=torch.complex64),mtr((n_y_sum,n_molecule))) / 19.0;
+CTF_k_p_r_xcor_kk__ = torch.reshape(torch.tensor([1,-2,-3,-2,4,-5,-3,-5,6]).to(dtype=torch.float32),mtr((3,3))) / 7.0;
+CTF_k_p_r_xcor_kk__ = torch.reshape(torch.tensor([1,2,3,2,4,6,3,6,9]).to(dtype=torch.float32),mtr((3,3))) / 7.0; #%<-- [1;2;3]*[1,2,3]/7.0 ;
+delta_sigma = 0.15;
+pm_delta_integral_tolerance = 1e-9;
+#%%%%%%%%;
+(
+    X_kk__,
+    X_weight_r_,
+    X_ori_kk__,
+    X_tau_kk__,
+    weight_so3,
+) = principled_marching_cost_matrix_7(
+    n_k_p_r,
+    k_p_r_,
+    weight_k_p_r_,
+    l_max_,
+    n_molecule,
+    molecule_density_,
+    a_k_Y_ykv__,
+    CTF_k_p_r_xcor_kk__,
+    delta_sigma,
+    pm_delta_integral_tolerance,
+)[:5];
+#%%%%%%%%;
+
+#%%%%%%%%;
+#% disp(num2str(X_kk__(:),';%+0.16f ...\n'));
+#%%%%%%%%;
+static_X_kk__ = torch.tensor([
+    +1.1715664289213095,
+    -0.4917345101851578,
+    +0.0037474319765092,
+    -0.4917345101851578,
+    +60.7408340504089423,
+    -2.8076460707416753,
+    +0.0037474319765092,
+    -2.8076460707416855,
+    +581.1505841190713681,
+]).to(dtype=torch.float32);
+static_X_kk__ = torch.reshape(static_X_kk__,mtr((3,3)));
+fnorm_disp(flag_verbose,'static_X_kk__',static_X_kk__,'X_kk__',X_kk__,'%<-- should be zero');
+#%%%%%%%%;
+static_X_weight_r_ = torch.tensor([
+    +0.25,
+    +0.50,
+    +0.75,
+]).to(dtype=torch.float32);
+fnorm_disp(flag_verbose,'static_X_weight_r_',static_X_weight_r_,'X_weight_r_',X_weight_r_,'%<-- should be zero');
+#%%%%%%%%;
+#% disp(num2str(X_ori_kk__(:),';%+0.16f ...\n'));
+#%%%%%%%%;
+static_X_ori_kk__ = torch.tensor([
+    +0.0074198654531064 + 0.0000000000000000*i,
+    -0.0184012663237040 + 0.0001978630787495*i,
+    +0.0289374752671151 - 0.0467451523545706*i,
+    -0.0184012663237040 - 0.0001978630787495*i,
+    +0.3846458250890383 + 0.0000000000000000*i,
+    -0.1050652948159873 - 0.2510882469331222*i,
+    +0.0289374752671151 + 0.0467451523545706*i,
+    -0.1050652948159873 + 0.2510882469331222*i,
+    +3.6801790660862688 + 0.0000000000000000*i,
+]).to(dtype=torch.complex64);
+static_X_ori_kk__ = torch.reshape(static_X_ori_kk__,mtr((3,3)));
+fnorm_disp(flag_verbose,'static_X_ori_kk__',static_X_ori_kk__,'X_ori_kk__',X_ori_kk__,'%<-- should be zero');
+#%%%%%%%%;
+#% disp(num2str(X_tau_kk__(:),';%+0.16f ...\n'));
+#%%%%%%%%;
+static_X_tau_kk__ = torch.tensor([
+    +0.3905660625678416 + 0.0000000000000000*i,
+    -0.4061887050705553 - 0.2812075650488459*i,
+    +1.8278491728174984 - 2.2496605203907674*i,
+    -0.4061887050705553 + 0.2812075650488459*i,
+    +0.6249057001085466 + 0.0000000000000000*i,
+    -0.2812075650488459 + 3.6556983456349967*i,
+    +1.8278491728174984 + 2.2496605203907674*i,
+    -0.2812075650488459 - 3.6556983456349967*i,
+    +21.5123787262367188 + 0.0000000000000000*i,
 ]).to(dtype=torch.complex64);
 static_X_tau_kk__ = torch.reshape(static_X_tau_kk__,mtr((3,3)));
 fnorm_disp(flag_verbose,'static_X_tau_kk__',static_X_tau_kk__,'X_tau_kk__',X_tau_kk__,'%<-- should be zero');
